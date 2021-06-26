ca9a21e0bb28b8d7cf564d7cecaeb4f7ad94b34d
jciglesias <jiglesia@student.42.fr>
2021-06-26 19:28:30 +0200

    ex07 handle empty strings

--- module01/ex07/main.cpp
+++ module01/ex07/main.cpp
@@ -6,7 +6,7 @@
 //   By: jiglesia <jiglesia@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2021/06/25 19:22:58 by jiglesia          #+#    #+#             //
-//   Updated: 2021/06/26 00:22:16 by jiglesia         ###   ########.fr       //
+//   Updated: 2021/06/26 19:27:26 by jiglesia         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -18,9 +18,15 @@ int	main(int ac, char **av)
 
 	if (ac == 4)
 	{
+		if (av[1][0] == 0 || av[2][0] == 0 || av[3][0] == 0)
+			return 0;
 		fstm.setFilename(av[1]);
 		fstm.setS1(av[2]);
 		fstm.setS2(av[3]);
 		fstm.replace();
+		return 0;
 	}
+	else
+		std::cout << "Bad arguments\n";
+	return 1;
 }
